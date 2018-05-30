d48b001f22335e11c510149040b1a07a17afe728
Ostaptom <tomkevicho@gmail.com>
2018-05-30 12:45:31 +0300

    'started pizda'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,39 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './source/header/header.component';
+import { FooterComponent } from './source/footer/footer.component';
+import { MainComponent } from './home/main/main.component';
+
+
+const routes: Routes = [
+  {
+    path: '', component: AppComponent, children: [
+      {
+        path:'', component: HomeComponent, children: [
+          {
+            path: '', component: MainComponent
+          },
+        ]
+      }
+    ]
+  }
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes, {useHash: true}),
   ],
   providers: [],
   bootstrap: [AppComponent]
