2fbbc4ba0475f0171232b36464fa7726b7830092
BillyTheWalker <bilevych85@gmail.com>
2018-06-14 21:17:53 +0300

    fix

--- src/shared/config/url.ts
+++ src/shared/config/url.ts
@@ -1,2 +1,2 @@
-export const url = 'http://localhost:7778';
-// export const url = 'http://mplus.hopto.org:7778';
+// export const url = 'http://localhost:7778';
+export const url = 'http://mplus.hopto.org:7778';
